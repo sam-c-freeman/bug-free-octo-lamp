f06f1dfcfd15495f930d32df2eff1a2e792f2fd6
Sam Freeman <samcfreeman888@gmail.com>
2022-10-08 16:58:49 -0500

    Started building form.  None of the logic is there

--- src/components/AddDrinkForm/AddDrinkForm.jsx
+++ src/components/AddDrinkForm/AddDrinkForm.jsx
@@ -1,9 +1,43 @@
 import './AddDrinkForm.css';
+import Stack from '@mui/material/Stack';
+import TextField from '@mui/material/TextField';
 
 function AddDrinkForm (){
     return(
-        <p>Hi!</p>
+        <Stack 
+            spacing={2}
+            alignItems="center"
+            >
+            <TextField 
+                id="name" 
+                variant="outlined" 
+                placeholder="Name"
+                style={{backgroundColor: "white"}}
+                sx={{width: 300}}
+                 />
+            <TextField 
+                id="description" 
+                variant="outlined"
+                placeholder="Description"
+                style={{backgroundColor: "white"}}
+                sx={{width: 300}} />
+            <TextField 
+                id="notes" 
+                variant="outlined"
+                placeholder="Notes"
+                style={{backgroundColor: "white"}}
+                sx={{width: 300}} />
+                <TextField 
+                id="imageURL" 
+                variant="outlined"
+                placeholder="Image URL"
+                style={{backgroundColor: "white"}}
+                sx={{width: 300}} />
+             
+        </Stack>
     )
 }
 
 export default AddDrinkForm;
+
+//Notes to self: Should I have an input label?
