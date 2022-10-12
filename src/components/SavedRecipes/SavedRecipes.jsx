d7baeaa01af35852ab5abec1312dd718a20d617d
Sam Freeman <samcfreeman888@gmail.com>
2022-10-12 11:22:36 -0500

    fixed rendering issue on saved recipes page

--- src/components/SavedRecipes/SavedRecipes.jsx
+++ src/components/SavedRecipes/SavedRecipes.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
-
+import Stack from '@mui/material/Stack';
 
 //is this for saved recipes and other is explore?
 
@@ -21,17 +21,39 @@ function SavedRecipesList () {
     const handleDetailsClick = (id) => {
         history.push(`/savedrecipes/${id}`)
     }
+
+    const goToExplore = () =>{
+        history.push('/explore')
+    }
     
     return(
         <>
-        {savedRecipes.map(recipe =>(
-            <div key={recipe.id}>
-                <h2>{recipe.name}</h2>
-                <img src={recipe.image_url} onClick={() => handleDetailsClick(recipe.id)}></img>
-            </div>
-            
-        ))}
-        </>
+        {savedRecipes.length === 0 ? (
+            <>
+                <Stack alignItems="center" spacing={2}>
+                    <h2>Search for Drinks</h2> 
+                    <input 
+                        className="btn" 
+                        type="submit" 
+                        name="submit" 
+                        value="Explore Page"
+                        onClick={goToExplore} />
+                </Stack>
+                    </>
+
+                )  : (  //if there are saved recipes, render them:
+        
+                <>
+                    {savedRecipes.map(recipe =>(
+                        <div key={recipe.id}>
+                            <h2>{recipe.name}</h2>
+                            <img src={recipe.image_url} onClick={() => handleDetailsClick(recipe.id)}></img>
+                        </div>
+                        
+                    ))}
+                </>
+                )}
+            </>
     )
 }
 
